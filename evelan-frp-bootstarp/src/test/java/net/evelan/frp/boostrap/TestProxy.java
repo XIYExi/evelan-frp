fbd3eaba0b46400314aacd345de7e6f6b48dd9d8
Zhou7 <Zhou730428@outlook.com>
2026-02-06 17:56:07 +0800

    准备写aop

---
+++ evelan-frp-bootstarp/src/test/java/net/evelan/frp/boostrap/TestProxy.java
@@ -0,0 +1,9 @@
+package net.evelan.frp.boostrap;
+
+import java.lang.reflect.Proxy;
+
+public class TestProxy {
+    public static void main(String[] args) {
+        // Proxy.newProxyInstance()
+    }
+}
