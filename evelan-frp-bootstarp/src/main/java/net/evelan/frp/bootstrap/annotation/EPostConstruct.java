16a3bd5250bdae7c0ac6c8ec868e056003cf59ce
Zhou7 <Zhou730428@outlook.com>
2026-02-06 16:02:03 +0800

    提交postconstruct

---
+++ evelan-frp-bootstarp/src/main/java/net/evelan/frp/bootstrap/annotation/EPostConstruct.java
@@ -0,0 +1,12 @@
+package net.evelan.frp.bootstrap.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 仿 Spring 的 @PostConstruct 注解，用于在依赖注入完成后执行初始化方法
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface EPostConstruct {
+}
