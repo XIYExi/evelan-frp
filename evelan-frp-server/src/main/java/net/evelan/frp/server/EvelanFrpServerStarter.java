3dbd71c771c4abcc6a84b2d4062f31514043d9d4
Zhou7 <Zhou730428@outlook.com>
2026-02-06 17:27:03 +0800

    删除测试代码

--- evelan-frp-server/src/main/java/net/evelan/frp/server/EvelanFrpServerStarter.java
+++ evelan-frp-server/src/main/java/net/evelan/frp/server/EvelanFrpServerStarter.java
@@ -1,15 +1,11 @@
 package net.evelan.frp.server;
 
 import net.evelan.frp.bootstrap.core.AssembleApplicationContext;
-import net.evelan.frp.bootstrap.repro.MyComponent;
 
 public class EvelanFrpServerStarter {
     public static void main(String... args) {
         AssembleApplicationContext context = new AssembleApplicationContext();
         TestController controller = (TestController) context.getBean(TestController.class);
         controller.test();
-        MyComponent myComponent = (MyComponent) context.getBean(MyComponent.class);
-        String myString = myComponent.getMyString();
-        System.out.println(myString);
     }
 }
